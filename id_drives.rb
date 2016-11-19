faca6273d682e4a976229e3fb5fc6e787d0cd82a
Carl Mercier <carl@carlmercier.com>
2016-11-18 22:47:20 -0500

    Added more details

--- id_drives.rb
+++ id_drives.rb
@@ -1,10 +1,15 @@
+#!/usr/bin/env ruby
+
+# This script identifies local drives and to which interface they're attached.
+# This was tested on Linux (Slackware) only.
+
 require 'yaml'
-lspci = []
+lspci = {}
 drives = {}
 
 `lspci`.each_line do |l|
   m = l.match(/(\S*)\s(.*)/)
-  lspci << { m[1] => m[2] }
+  lspci[m[1]] = m[2]
 end
 
 ('a'..'z').each do |letter|
@@ -16,8 +21,22 @@ end
   drives[drive][:vendor]   = `udevadm info -q all -n #{drive} | grep ID_VENDOR`.match(regexp)[1] rescue nil
   drives[drive][:model]    = `udevadm info -q all -n #{drive} | grep ID_MODEL`.match(regexp)[1] rescue nil
   drives[drive][:serial]   = `udevadm info -q all -n #{drive} | grep ID_SERIAL_SHORT`.match(regexp)[1] rescue nil
-  drives[drive][:pci_addr] = drives[drive][:devpath].match(/\/devices\/pci\d{4}:\d{2}\/([^\/]*)/)[1] rescue nil
+  drives[drive][:pci_addr] = drives[drive][:devpath].match(/\/devices\/pci\d{4}:\d{2}\/([^\/]*)/)[1].sub(/^0000\:/, '') rescue nil
+end
+
+drives_by_pci_address = {}
+
+drives.each_key do |drive|
+  pci_addr = drives[drive][:pci_addr]
+  drives_by_pci_address[pci_addr] = {} if drives_by_pci_address[pci_addr].nil?
+  if drives_by_pci_address[pci_addr][:drives].nil?
+    drives_by_pci_address[pci_addr][:id] = lspci[pci_addr] || "not found"
+    drives_by_pci_address[pci_addr][:drives] = []
+  end
+  drives_by_pci_address[pci_addr][:drives] << drive
 end
 
 puts drives.to_yaml
-nil
+puts "------------------------------"
+puts drives_by_pci_address.to_yaml
+
